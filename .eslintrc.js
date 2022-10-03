0375fd353ca2b5bff50858e7c6cfcc60d841d643
hiko dong <dong.pt@hikotech.io>
2022-10-03 09:37:40 +0700

    remove eslink

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
+  },
+  'extends': [
+    'plugin:vue/essential'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs'
-    // 'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {
-    'vue/singleline-html-element-content-newline': 'off',
-    'no-console': 'off',
-    'vue/no-v-html': 'off'
   }
 }
